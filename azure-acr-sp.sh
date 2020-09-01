12ee6871fafa0a6813671bdd948c869c91749e22
Paul Yu <pauldotyu@gmail.com>
2020-08-31 21:26:26 -0700

    update

--- azure-acr-sp.sh
+++ azure-acr-sp.sh
@@ -8,7 +8,7 @@ az ad sp create-for-rbac -n grouper0822sp
 # Registry, and the SERVICE_PRINCIPAL_ID is the service principal's 'appId' or
 # one of its 'servicePrincipalNames' values.
 ACR_NAME=grouper0822
-SERVICE_PRINCIPAL_ID=d53c0ab5-6623-4351-acbb-dba01116f97f
+SERVICE_PRINCIPAL_ID=
 
 # Populate value required for subsequent command args
 ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query id --output tsv)
